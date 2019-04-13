34b3f060932d6769c4495201cc1eece67871d2f8
Baudouin Théobald <baudouin.theobald@student-cs.fr>
2019-04-13 18:26:18 +0200

    added button style

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -4,7 +4,7 @@ var HTML = document.querySelector("html");
 var homeButton = document.getElementById("homeButton");
 homeButton.addEventListener("click", function(){location.assign("index.html");});
 
-//setting up the links
+//setting up the icon links
 var github = document.getElementById("githubLink");
 github.addEventListener("click", function(){location.assign("https://github.com/Bauthe");});
 github.addEventListener("mouseenter", function(){github.setAttribute("src", "assets/imgs/icons/github-grey.png");});
@@ -14,10 +14,3 @@ var linkedin = document.getElementById("linkedinLink");
 linkedin.addEventListener("click", function(){location.assign("https://www.linkedin.com/in/baudouin-th%C3%A9obald-01aa22181/");});
 linkedin.addEventListener("mouseenter", function(){linkedin.setAttribute("src", "assets/imgs/icons/linkedin-grey.png");});
 linkedin.addEventListener("mouseleave", function(){linkedin.setAttribute("src", "assets/imgs/icons/linkedin-white.png");});
-
-//setting up the border button
-var borderStyle = document.createElement("style");
-borderStyle.textContent = "#borderButton{border: solid 2px}";
-var borderButton = document.getElementById("borderButton");
-borderButton.addEventListener("mousedown", function(){HTML.appendChild(borderStyle);})
-borderButton.addEventListener("mouseup", function(){HTML.removeChild(borderStyle);})
