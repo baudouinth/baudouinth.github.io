e21bf913977c622cc48a214b8efb5f7bf449cb67
Baudouin Théobald <baudouin.theobald@student-cs.fr>
2019-04-14 23:19:14 +0200

    improved buttons

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,16 +1,14 @@
 var HTML = document.querySelector("html");
 
-//setting up the home button
-var homeButton = document.getElementById("homeButton");
-homeButton.addEventListener("click", function(){location.assign("index.html");});
-
 //setting up the icon links
-var github = document.getElementById("githubLink");
-github.addEventListener("click", function(){location.assign("https://github.com/Bauthe");});
+var github = document.getElementById("githubLink").children[0];
 github.addEventListener("mouseenter", function(){github.setAttribute("src", "assets/imgs/icons/github-grey.png");});
 github.addEventListener("mouseleave", function(){github.setAttribute("src", "assets/imgs/icons/github-white.png");});
+github.addEventListener("mousedown", function(event){if (event.button == 0){github.setAttribute("src", "assets/imgs/icons/github-black.png");}});
+github.addEventListener("mouseup", function(event){if (event.button == 0){github.setAttribute("src", "assets/imgs/icons/github-white.png");}});
 
-var linkedin = document.getElementById("linkedinLink");
-linkedin.addEventListener("click", function(){location.assign("https://www.linkedin.com/in/baudouin-th%C3%A9obald-01aa22181/");});
+var linkedin = document.getElementById("linkedinLink").children[0];
 linkedin.addEventListener("mouseenter", function(){linkedin.setAttribute("src", "assets/imgs/icons/linkedin-grey.png");});
 linkedin.addEventListener("mouseleave", function(){linkedin.setAttribute("src", "assets/imgs/icons/linkedin-white.png");});
+linkedin.addEventListener("mousedown", function(event){if (event.button == 0){linkedin.setAttribute("src", "assets/imgs/icons/linkedin-black.png");}});
+linkedin.addEventListener("mouseup", function(event){if (event.button == 0){linkedin.setAttribute("src", "assets/imgs/icons/linkedin-white.png");}});
