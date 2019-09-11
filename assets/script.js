af6ed33df5394e8cb696ec606af51c4a076017cd
Baudouin Théobald <49082598+Bauthe@users.noreply.github.com>
2019-09-11 04:10:20 +0200

    Delete script.js

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-let input = document.getElementsByTagName("input")[0];
-input.addEventListener('keypress', function (e) {
-    let key = e.which || e.keyCode;
-    if (key === 13) {
-        let search = input.value.replace("%", "%25").replace("@", "%40").replace("#", "%23").replace("$", "%24").replace("&", "%26").replace("=", "%3D").replace("+", "%2B");
-        location = "https://www.google.com/search?client=firefox-b-d&q=" + search;
-    }
-});
